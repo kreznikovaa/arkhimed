3367c39074c40d6046d68ee314fd395f0b8abdf2
Kristina <2022reznikova.ka@gmail.com>
2025-05-10 23:41:40 +0200

    картинки 1

--- static/js/menu.js
+++ static/js/menu.js
@@ -49,7 +49,7 @@ document.addEventListener("DOMContentLoaded", () => {
       const back = card.querySelector(".course-card-back");
   
       if (front) {
-        front.style.background = `linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.3)), url('${img}') center/cover no-repeat`;
+        front.style.background = `url('${img}') center/cover no-repeat`;
         front.style.color = "white";
       }
   
